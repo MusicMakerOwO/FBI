7e11a9214f9a103ecacc9b6aba591d796482f5a8
Music Maker <joshuanejdl@gmail.com>
2024-12-28 19:02:44 -0600

    feat: Additional typings for database and message stripping

--- TS/typings.d.ts
+++ TS/typings.d.ts
@@ -6,6 +6,111 @@ import ShardManager from './Utils/Sharding/ShardManager';
 import Collector from './Utils/Overrides/Collector';
 
 import Log from './Utils/Logs';
+import CachePool from './Utils/Caching/CachePool';
+
+type AssetDimensions = {
+	width: number;
+	height: number;
+}
+
+export interface BasicAsset {
+	id: string;
+	url: string;
+	name: string;
+	extension: string;
+	dimensions: AssetDimensions | null; // Things like text files don't have dimensions lol
+}
+
+export interface EmojiAsset extends BasicAsset {
+	animated: boolean;
+}
+
+// Doesn't add anything but it's here for consistency
+export interface AttachmentAsset extends BasicAsset { }
+
+export interface StickerAsset extends BasicAsset {
+	description: string | null;
+	format: StickerFormats; // number, index starting at 1, corrected in parser
+}
+
+export enum StickerFormats {
+	PNG = 1,
+	APNG = 2,
+	Lottie = 3,
+	GIF = 4
+}
+
+export interface BasicEmbed {
+	// This is here for completeness and avoid nested loops
+	// It's a little bit more ram but the coede is simpler
+	id: string;
+	messageID: string;
+
+	title: string | null;
+	description: string | null;
+	url: string | null;
+	timestamp: string | null;
+	color: number | null;
+
+	// Unwraping objects make it easier for the database
+	footer_text: string | null;
+	footer_icon: string | null;
+
+	thumbnail_url: string | null;
+
+	image_url: string | null;
+
+	author_name: string | null;
+	author_url: string | null;
+	author_icon: string | null;
+
+	fields: EmbedField[];
+}
+
+export interface EmbedField {
+	name: string;
+	value: string;
+	inline?: boolean;
+}
+
+export interface User {
+	id: string;
+	username: string;
+	icon: BasicAsset; // default avatars will be calculated by the parser
+	bot: boolean;
+}
+
+export interface Member extends User {
+	guildID: string;
+	joinedAt: string | null; // ISO 8601 - Only if user joins while bot is in the guild
+	leftAt: string | null; // Same as above but for leaving 
+}
+
+export interface Channel {
+	guildID: string; // Again with the loop prevention
+	id: string;
+	name: string;
+	type: number;
+	parentID: string | null; // null = no category
+}
+
+export interface Guild {
+	id: string;
+	name: string;
+	icon: BasicAsset | null;
+}
+
+export interface BasicMessage {
+	guild: Guild; // We are ignoring DMs so always present
+	channel: Channel,
+	user: User & { joinedAt: string }, // It can't possibly be null because how can you send a message outside of the server?
+	id: string;
+	content: string;
+	embeds: BasicEmbed[];
+	emojis: EmojiAsset[];
+	attachments: AttachmentAsset[];
+	sticker: StickerAsset | null;
+}
 
 // This is the outlier lol
 export interface EventFile {
@@ -87,7 +192,13 @@ export interface MicroClient extends Client {
 	menus: Map<string, ComponentFile>;
 	modals: Map<string, ComponentFile>;
 	messages: Map<string, MessageFile>;
+
+	// Caching
+	messageCache: CachePool<BasicMessage>;
+	downloadQueue: DownloadEntry[];
 }
+
+export declare type DownloadEntry = [table: string, id: string, url: string];
 
 export interface MicroInteractionResponse extends InteractionReplyOptions {
 	hidden?: boolean;
