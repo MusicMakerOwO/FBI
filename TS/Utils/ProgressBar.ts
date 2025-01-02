1e70f7a5ab0539fc4e94de2e7fa8d18d12c17245
Music Maker <joshuanejdl@gmail.com>
2025-01-01 19:45:31 -0600

    feat: Custom progress bar utility

---
+++ TS/Utils/ProgressBar.ts
@@ -0,0 +1,11 @@
+const PROGRESS = [' ', '⡀', '⣀', '⣄', '⣤', '⣦', '⣶', '⣷', '⣿'];
+
+export default function ProgressBar(progress: number, max: number) {
+	if (progress > max) progress = max;
+	
+	const percent = Math.floor((progress / max) * 100);
+	const full = Math.floor(progress / max * PROGRESS.length);
+	const remainder = Math.floor((progress / max * PROGRESS.length - full) * 8);
+	const bar = `${PROGRESS[PROGRESS.length - 1].repeat(full)}${PROGRESS[remainder]}${' '.repeat(PROGRESS.length - full)}`;
+	return `[ ${bar}] ${percent}%`;
+}
